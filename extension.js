e12991e2ed5fba609214d52a037099a4a8db3407
Artem Babii <arthem.babij@gmail.com>
2024-09-29 10:52:48 +0300

    add gnome 47 support

--- extension.js
+++ extension.js
@@ -10,75 +10,79 @@ const ICON_OFF = "network-vpn-offline-symbolic";
 const COMMAND = "ip link show cscotun0";
 
 const MyIndicator = GObject.registerClass(
-    class MyIndicator extends QuickSettings.SystemIndicator {
-        _init() {
-            super._init();
-            this._indicator = this._addIndicator();
-        }
+  class MyIndicator extends QuickSettings.SystemIndicator {
+    _init() {
+      super._init();
+      this._indicator = this._addIndicator();
     }
+  },
 );
 
 export default class IndicatorExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
-        this._settings = null;
-        this._indicator = null;
-        this._checkLoop = null;
-    }
+  constructor(metadata) {
+    super(metadata);
+    this._settings = null;
+    this._indicator = null;
+    this._checkLoop = null;
+  }
 
-    async _runCommand() {
-        const iconOn = this._settings.get_string("icon-name-enabled") || ICON_ON;
-        const iconOff = this._settings.get_string("icon-name-disabled") || ICON_OFF;
-        const showIconOff = this._settings.get_boolean("show-disabled-icon");
-        const command = this._settings.get_string("cli-command") || COMMAND;
-        const [, parsedCommand] = GLib.shell_parse_argv(command);
+  async _runCommand() {
+    const iconOn = this._settings.get_string("icon-name-enabled") || ICON_ON;
+    const iconOff = this._settings.get_string("icon-name-disabled") || ICON_OFF;
+    const showIconOff = this._settings.get_boolean("show-disabled-icon");
+    const command = this._settings.get_string("cli-command") || COMMAND;
+    const [, parsedCommand] = GLib.shell_parse_argv(command);
 
-        try {
-            const proc = Gio.Subprocess.new(parsedCommand, Gio.SubprocessFlags.NONE);
-            const success = await proc.wait_check_async(null);
-            if (success) {
-                this._indicator._indicator.icon_name = iconOn;
-                this._indicator._indicator.visible = true;
-            } else {
-                if (showIconOff) {
-                    this._indicator._indicator.icon_name = iconOff;
-                    this._indicator._indicator.visible = true;
-                } else {
-                    this._indicator._indicator.visible = false;
-                }
-            }
-        } catch (e) {
-            if (showIconOff) {
-                this._indicator._indicator.icon_name = iconOff;
-                this._indicator._indicator.visible = true;
-            } else {
-                this._indicator._indicator.visible = false;
-            }
+    try {
+      const proc = Gio.Subprocess.new(parsedCommand, Gio.SubprocessFlags.NONE);
+      const success = await proc.wait_check_async(null);
+      if (success) {
+        this._indicator._indicator.icon_name = iconOn;
+        this._indicator._indicator.visible = true;
+      } else {
+        if (showIconOff) {
+          this._indicator._indicator.icon_name = iconOff;
+          this._indicator._indicator.visible = true;
+        } else {
+          this._indicator._indicator.visible = false;
         }
+      }
+    } catch (e) {
+      if (showIconOff) {
+        this._indicator._indicator.icon_name = iconOff;
+        this._indicator._indicator.visible = true;
+      } else {
+        this._indicator._indicator.visible = false;
+      }
     }
+  }
 
-    enable() {
-        this._settings = this.getSettings();
-        this._indicator = new MyIndicator();
+  enable() {
+    this._settings = this.getSettings();
+    this._indicator = new MyIndicator();
 
-        Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
+    Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
 
+    this._runCommand();
+    this._periodSec = this._settings.get_int("period-sec");
+    this._checkLoop = GLib.timeout_add_seconds(
+      GLib.PRIORITY_DEFAULT,
+      this._periodSec,
+      () => {
         this._runCommand();
-        this._periodSec = this._settings.get_int("period-sec");
-        this._checkLoop = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, this._periodSec, () => {
-            this._runCommand();
-            return GLib.SOURCE_CONTINUE;
-        });
+        return GLib.SOURCE_CONTINUE;
+      },
+    );
+  }
+
+  disable() {
+    if (this._checkLoop) {
+      GLib.Source.remove(this._checkLoop);
+      this._checkLoop = null;
     }
 
-    disable() {
-        if (this._checkLoop) {
-            GLib.Source.remove(this._checkLoop);
-            this._checkLoop = null;
-        }
-
-        this._settings = null;
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+    this._settings = null;
+    this._indicator.destroy();
+    this._indicator = null;
+  }
 }
--- prefs.js
+++ prefs.js
@@ -4,111 +4,136 @@ import Gio from "gi://Gio";
 import Gtk from "gi://Gtk";
 
 export default class IndicatorPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        const settings = this.getSettings();
-        const page = new Adw.PreferencesPage();
-        const group = new Adw.PreferencesGroup();
-        page.add(group);
+  fillPreferencesWindow(window) {
+    const settings = this.getSettings();
+    const page = new Adw.PreferencesPage();
+    const group = new Adw.PreferencesGroup();
+    page.add(group);
 
-        // Command
-        const rowCommand = new Adw.ActionRow({
-            title: "Command",
-            subtitle: "It must return 0 exit code on success",
-        });
-        group.add(rowCommand);
+    // Command
+    const rowCommand = new Adw.ActionRow({
+      title: "Command",
+      subtitle: "It must return 0 exit code on success",
+    });
+    group.add(rowCommand);
 
-        const entryCommand = new Gtk.Entry({
-            placeholder_text: "ip link show cscotun0",
-            text: settings.get_string("cli-command"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const entryCommand = new Gtk.Entry({
+      placeholder_text: "ip link show cscotun0",
+      text: settings.get_string("cli-command"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("cli-command", entryCommand, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "cli-command",
+      entryCommand,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowCommand.add_suffix(entryCommand);
-        rowCommand.activatable_widget = entryCommand;
+    rowCommand.add_suffix(entryCommand);
+    rowCommand.activatable_widget = entryCommand;
 
-        // Check Frequency
-        const rowPeriod = new Adw.ActionRow({
-            title: "Check Frequency",
-            subtitle: "Seconds",
-        });
-        group.add(rowPeriod);
+    // Check Frequency
+    const rowPeriod = new Adw.ActionRow({
+      title: "Check Frequency",
+      subtitle: "Seconds",
+    });
+    group.add(rowPeriod);
 
-        const periodAdjustment = new Gtk.Adjustment({
-            value: settings.get_int("period-sec"),
-            lower: 1,
-            upper: 300,
-            step_increment: 1,
-        });
+    const periodAdjustment = new Gtk.Adjustment({
+      value: settings.get_int("period-sec"),
+      lower: 1,
+      upper: 300,
+      step_increment: 1,
+    });
 
-        const periodSpinButton = new Gtk.SpinButton({
-            adjustment: periodAdjustment,
-            numeric: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.END,
-        });
+    const periodSpinButton = new Gtk.SpinButton({
+      adjustment: periodAdjustment,
+      numeric: true,
+      valign: Gtk.Align.CENTER,
+      halign: Gtk.Align.END,
+    });
 
-        settings.bind("period-sec", periodSpinButton.get_adjustment(), "value", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "period-sec",
+      periodSpinButton.get_adjustment(),
+      "value",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowPeriod.add_suffix(periodSpinButton);
-        rowPeriod.activatable_widget = periodSpinButton;
+    rowPeriod.add_suffix(periodSpinButton);
+    rowPeriod.activatable_widget = periodSpinButton;
 
-        // Enabled Icon Name
-        const rowIconOn = new Adw.ActionRow({
-            title: "Enabled Icon Name",
-            subtitle: "/usr/share/icons/",
-        });
-        group.add(rowIconOn);
+    // Enabled Icon Name
+    const rowIconOn = new Adw.ActionRow({
+      title: "Enabled Icon Name",
+      subtitle: "/usr/share/icons/",
+    });
+    group.add(rowIconOn);
 
-        const iconOnEntry = new Gtk.Entry({
-            placeholder_text: "network-vpn-symbolic",
-            text: settings.get_string("icon-name-enabled"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const iconOnEntry = new Gtk.Entry({
+      placeholder_text: "network-vpn-symbolic",
+      text: settings.get_string("icon-name-enabled"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("icon-name-enabled", iconOnEntry, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "icon-name-enabled",
+      iconOnEntry,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowIconOn.add_suffix(iconOnEntry);
-        rowIconOn.activatable_widget = iconOnEntry;
+    rowIconOn.add_suffix(iconOnEntry);
+    rowIconOn.activatable_widget = iconOnEntry;
 
-        // Show Disabled Icon
-        const rowShowIconOff = new Adw.ActionRow({
-            title: "Show Disabled Icon",
-        });
-        group.add(rowShowIconOff);
+    // Show Disabled Icon
+    const rowShowIconOff = new Adw.ActionRow({
+      title: "Show Disabled Icon",
+    });
+    group.add(rowShowIconOff);
 
-        const toggleIconOff = new Gtk.Switch({
-            active: settings.get_boolean("show-disabled-icon"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleIconOff = new Gtk.Switch({
+      active: settings.get_boolean("show-disabled-icon"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("show-disabled-icon", toggleIconOff, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "show-disabled-icon",
+      toggleIconOff,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowShowIconOff.add_suffix(toggleIconOff);
-        rowShowIconOff.activatable_widget = toggleIconOff;
+    rowShowIconOff.add_suffix(toggleIconOff);
+    rowShowIconOff.activatable_widget = toggleIconOff;
 
-        // Disabled Icon Name
-        const rowIconOff = new Adw.ActionRow({
-            title: "Disabled Icon Name",
-            subtitle: "/usr/share/icons/",
-        });
-        group.add(rowIconOff);
+    // Disabled Icon Name
+    const rowIconOff = new Adw.ActionRow({
+      title: "Disabled Icon Name",
+      subtitle: "/usr/share/icons/",
+    });
+    group.add(rowIconOff);
 
-        const iconOffEntry = new Gtk.Entry({
-            placeholder_text: "network-vpn-offline-symbolic",
-            text: settings.get_string("icon-name-disabled"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const iconOffEntry = new Gtk.Entry({
+      placeholder_text: "network-vpn-offline-symbolic",
+      text: settings.get_string("icon-name-disabled"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("icon-name-disabled", iconOffEntry, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "icon-name-disabled",
+      iconOffEntry,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowIconOff.add_suffix(iconOffEntry);
-        rowIconOff.activatable_widget = iconOffEntry;
+    rowIconOff.add_suffix(iconOffEntry);
+    rowIconOff.activatable_widget = iconOffEntry;
 
-        window.add(page);
-    }
+    window.add(page);
+  }
 }
